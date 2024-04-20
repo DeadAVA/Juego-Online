d58e3be647d77ec37c9ce85b62aa1d1cb271f532
Dead <107895779+DeadAVA@users.noreply.github.com>
2024-04-19 23:51:59 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,45 +1,60 @@
-import socket
-import sys
 import pygame as py
+import sys
 import pickle
+import socket
 from constant import *
-from game import Tablero
+
 
 # Definir el host y puerto al que se conectará el cliente
 HOST = '192.168.1.241'  # Dirección IP del servidor
 PORT = 65432        # Puerto del servidor
 
+
 class Cliente:
-    def __init__(self, direccion_servidor, puerto_servidor):
+    def __init__(self, host, puerto):
+        self.host = host
+        self.puerto = puerto
         self.cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.cliente_socket.connect((direccion_servidor, puerto_servidor))
-        self.game_loop()
+        self.cliente_socket.connect((self.host, self.puerto))
+        self.tablero = None
 
-    def game_loop(self):
-        while True:
-            # Recibe el estado del tablero del servidor
-            data = self.cliente_socket.recv(1024)
-            tablero = pickle.loads(data)
-            
-            # Lógica del juego (entrada del jugador)
-            for event in py.event.get():
-                if event.type == py.QUIT:
-                    py.quit()
-                    sys.exit()
-                elif event.type == py.MOUSEBUTTONDOWN:
-                    x, y = py.mouse.get_pos()
-                    fila = y // TAMANO_CUADRO
-                    columna = x // TAMANO_CUADRO
-                    tablero.marcar(fila, columna)
-            
-            # Envía la acción del jugador al servidor
-            self.cliente_socket.send(pickle.dumps((fila, columna)))
-            
-            # Verifica si el juego ha terminado
-            if tablero.game_over:
-                break
+    def recibir_estado(self):
+        data = self.cliente_socket.recv(1024)
+        self.tablero = pickle.loads(data)
 
+    def enviar_movimiento(self, fila, columna):
+        data = pickle.dumps((fila, columna))
+        self.cliente_socket.send(data)
+
+    def cerrar_conexion(self):
         self.cliente_socket.close()
 
-def client():
-    cliente = Cliente(HOST, PORT)
+def Game_online():
+    # Estructura del juego
+    py.init()
+
+    screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    py.display.set_caption("El gato")
+    clock = py.time.Clock()
+
+    cliente = Cliente('localhost', 65432)
+    game = True
+
+    while game:
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                cliente.cerrar_conexion()
+                py.quit()
+                sys.exit()
+            elif event.type == py.MOUSEBUTTONDOWN:
+                x, y = py.mouse.get_pos()
+                fila = y // TAMANO_CUADRO
+                columna = x // TAMANO_CUADRO
+                cliente.enviar_movimiento(fila, columna)
+
+        cliente.recibir_estado()
+        cliente.tablero.dibujar(screen)
+        py.display.flip()
+        clock.tick(60)
+
+    return
